1ebd3b2a2474afd7b130387404c2a4a7485141c8
Rayan Sahbani <pa62yjh@eduvaud.ch>
2026-01-14 16:25:55 +0100

    feat(Cours 1 sur le projet): J'ai commencé à faire mon rapport, j'ai rempli mon journal de travail et j'ai aussi commencé à coder

---
+++ src/OO-them-ALL/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OO_them_ALL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("••••••• BANCOMAT D' RS7 •••••••");
+            menu();
+            static void menu()
+            {
+                string numeroCart;
+                Console.WriteLine("••••••• MENU PRINCIPAL •••••••");
+                Console.Write("Numéro de carte (ex: 0212 XXXX XXXX XXXX) :  ");
+                numeroCart = Console.ReadLine();
+                
+
+
+            }
+
+        }
+    }
+}
