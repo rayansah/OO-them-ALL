cfad7cd063bebdcc6263c8b5b653f57bb6e7e235
rayansah <pa62yjh@gmail.com>
2026-01-21 16:29:33 +0100

    feat(verification des INPUT): verification du PIN et du numero carte ainsi le menu

--- src/OO-them-ALL/Program.cs
+++ src/OO-them-ALL/Program.cs
@@ -1,18 +1,74 @@
-﻿namespace OO_them_ALL
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+
+namespace OO_them_ALL
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("••••••• BANCOMAT D' RS7 •••••••");
+            
             menu();
             static void menu()
             {
                 string numeroCart;
-                Console.WriteLine("••••••• MENU PRINCIPAL •••••••");
+                string pinCart;
+                bool valueOkNumCarte;
+                bool valueOkPIN;
+                int pinCartValid = 0;
+                bool valid = true;
+                valueOkNumCarte = false;
+                valueOkPIN = false;
+
+                Console.Clear();
+                Console.WriteLine("\t══════════ BANCOMAT D' RS7 ══════════\n");
+                Console.WriteLine("\t══════════ MENU PRINCIPAL ══════════\n");
                 Console.Write("Numéro de carte (ex: 0212 XXXX XXXX XXXX) :  ");
                 numeroCart = Console.ReadLine();
+
+                while (valueOkNumCarte == false) 
+                {   
+                    
+                    
+                    if (long.TryParse(numeroCart, out long numeroCartValid))
+                    {
+                        Console.Write("\nCode PIN (ex: 11 XX XX) : ");
+                        pinCart = Console.ReadLine();
+                        while (valueOkPIN == false)
+                        {
+                            if (int.TryParse(pinCart, out pinCartValid))
+                            {
+                                Console.WriteLine("1");
+                                Console.WriteLine("2");
+                                Console.WriteLine("3");
+                                valueOkPIN=true;
+                            }
+                            else 
+                            {
+                                Console.Write("Votre numero de carte n'est pas correcte ! Merci de réessayer : ");
+                                valueOkPIN=false;
+                                pinCart = Console.ReadLine();
+                            }
+                        }
+                        
+                        valueOkNumCarte = true;
+                    }
+                    else 
+                    {
+                        Console.Write("Votre numero de carte n'est pas correcte ! Merci de réessayer : ");
+                        numeroCart = Console.ReadLine() ;
+                        valueOkNumCarte= false;
+                    }
+
+                }
                 
+                
+                
+                    
+                    
+
+                
+
 
 
             }
